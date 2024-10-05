8638fce6cc5accf1fd28525f1233a157f92ce20e
stefffffffffano <s323195@studenti.polito.it>
2024-10-05 14:16:31 +0200

    Try for solving a conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+      <div>Prova</div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
