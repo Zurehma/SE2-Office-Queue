f1979f42eaf40793ac51922587851684cd7aab2a
Luca Pes <s332943@studenti.polito.it>
2024-10-06 16:48:20 +0200

    backend refactor

--- server/index.mjs
+++ server/index.mjs
@@ -1,1 +1,35 @@
-//APIs definition in here
+// APIs definition in here
+
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import Utility from "./utilities.mjs";
+import AuthRoutes from "./routes/authRoutes.mjs";
+import ServiceRoutes from "./routes/serviceRoutes.mjs";
+
+const app = new express();
+const port = 3001;
+const baseURL = "/api";
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "25mb" }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+
+const authRoutes = new AuthRoutes(app);
+const serviceRoutes = new ServiceRoutes();
+
+authRoutes.initRoutes();
+serviceRoutes.initRoutes();
+
+app.use(baseURL + "/sessions", authRoutes.getRouter());
+app.use(baseURL + "/service", serviceRoutes.getRouter());
+app.use(Utility.errorHandler);
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
