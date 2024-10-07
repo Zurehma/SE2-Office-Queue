ec9c582b2347bae28c2d0f7c3e11f208ca1a9ccd
Zurehma Ayesha Rameez s323502 <s323502@studenti.polito.it>
2024-10-07 13:35:14 +0200

    AuthRoutes update

--- server/routes/authRoutes.mjs
+++ server/routes/authRoutes.mjs
@@ -74,7 +74,27 @@ function AuthRoutes(app) {
       }
     );
 
+    //Logout
+    this.router.delete(
+      "/logout",
+      (req,res) =>{
+        req.logout(()=>{
+          res.end();
+        })
+      }
+    )
 
+    //Get current user
+    this.router.get(
+      "/current",
+      (req,res)=>{
+        if(req.isAuthenticated()){
+          res.status(200).json(req.user);
+        } else {
+            res.status(401).json({message:'Unauthorized'});
+        }
+      }  
+    )
 
   };
 
