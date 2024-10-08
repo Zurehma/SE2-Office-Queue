a133d6597da724f6180c869985f08181a85ad39e
Luca Pes <s332943@studenti.polito.it>
2024-10-08 19:28:48 +0200

    added ticket dequeue API and queue manager implementation

--- server/dao/serviceDAO.mjs
+++ server/dao/serviceDAO.mjs
@@ -1,7 +1,58 @@
 // Module for the service's Data Access Object (DAO)
 
 import db from "../db/db.mjs";
+import Service from "../models/service.mjs";
 
-const ServiceDAO = {};
+/**
+ *
+ * @param {*} rows
+ * @returns
+ */
+const mapRowsToService = (rows) => {
+  return rows.map((row) => new Service(row.name, row.code, row.averageTime));
+};
+
+/**
+ *
+ * @returns
+ */
+const getServices = () => {
+  return new Promise((resolve, reject) => {
+    const query = "SELECT * FROM services";
+
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(mapRowsToService(rows));
+      }
+    });
+  });
+};
+
+/**
+ * 
+ * @param {*} counterID 
+ * @returns 
+ */
+const getServicesPerCounter = (counterID) => {
+  return new Promise((resolve, reject) => {
+    const query =
+      "SELECT name, averageTime, code FROM services s, servicesPerCounter sc WHERE s.serviceId == sc.serviceId AND sc.counterId = ?";
+
+    db.all(query, [counterID], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(mapRowsToService(rows));
+      }
+    });
+  });
+};
+
+function ServiceDAO() {
+  this.getServices = getServices;
+  this.getServicesPerCounter = getServicesPerCounter;
+}
 
 export default ServiceDAO;
---
+++ server/models/queue.mjs
@@ -0,0 +1,98 @@
+// Queue manager module
+
+import ServiceDAO from "../dao/serviceDAO.mjs";
+
+/**
+ * Queue manager class
+ */
+function QueueManager() {
+  this.queues = {};
+  this.serviceDAO = new ServiceDAO();
+
+  /**
+   * Constructor
+   */
+  this.init = async () => {
+    let services = await this.serviceDAO.getServices();
+
+    services.forEach((service) => (this.queues[service.code] = { service: service, queue: [] }));
+  };
+
+  /**
+   *
+   * @param {*} serviceCode The service code of the queue you want to enqueue the new ticket
+   * @returns
+   * - **undefined** if the service code does not exist
+   * - The ticket value that has added to the queue
+   */
+  this.enqueue = (serviceCode) => {
+    console.log(serviceCode, this.queues);
+    if (!this.queues.hasOwnProperty(serviceCode)) return undefined;
+
+    const ticket = serviceCode + (this.queues[serviceCode].queue.length + 1);
+    this.queues[serviceCode].queue.push(ticket);
+    return ticket;
+  };
+
+  /**
+   *
+   * @param {*} serviceCode The service code of the queue you want to dequeue the new ticket
+   * @returns
+   * - **undefined** if the service code does not exist or if the queue is empty
+   * - The ticket value that has been removed from the queue
+   */
+  this.dequeue = (serviceCode) => {
+    if (!this.queues.hasOwnProperty(serviceCode) || this.queues[serviceCode].queue.length === 0)
+      return undefined;
+
+    return this.queues[serviceCode].queue.shift();
+  };
+
+  /**
+   *
+   * @param {*} serviceCode The service code of the queue you want to check the length
+   * @returns
+   * - **undefined** if the service code does not exist
+   * - **true** if the length of the queue is 0 else **false**
+   */
+  this.isEmpty = (serviceCode) => {
+    if (!this.queues.hasOwnProperty(serviceCode)) return undefined;
+
+    return this.queues[serviceCode].queue.length === 0;
+  };
+
+  /**
+   *
+   * @param {*} serviceCode The service code of the queue you want to check the length
+   * @returns
+   * - **undefined** if the service code does not exist
+   * - The number of people in queue for the given service
+   */
+  this.length = (serviceCode) => {
+    if (!this.queues.hasOwnProperty(serviceCode)) return undefined;
+
+    return this.queues[serviceCode].queue.length;
+  };
+
+  /**
+   *
+   * @param {*} serviceCode The service code of the queue you want to check the length
+   * @returns
+   * - **undefined** if the service code does not exist
+   * - The waiting time based on the number of people in queue
+   */
+  this.waitingTime = (serviceCode) => {
+    if (!this.queues.hasOwnProperty(serviceCode)) return undefined;
+
+    return this.queues[serviceCode].queue.length * this.queues[serviceCode].service.averageTime;
+  };
+
+  /**
+   * Reset the queue manager
+   */
+  this.reset = () => {
+    this.queues = {};
+  };
+}
+
+export default QueueManager;
