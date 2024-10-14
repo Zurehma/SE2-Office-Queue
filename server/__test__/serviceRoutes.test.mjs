f71539a813ef23817fa61a7f10063e0b6e7b65ed
Zurehma Ayesha Rameez s323502 <s323502@studenti.polito.it>
2024-10-14 20:05:25 +0200

    Test Updates- Not working yet

--- server/__test__/serviceRoutes.test.mjs
+++ server/__test__/serviceRoutes.test.mjs
@@ -1,20 +1,73 @@
 import request from "supertest";
 import { app } from "../index.mjs";
 import ServiceDAO from "../dao/serviceDAO.mjs";
+import ServiceRoutes from "../routes/serviceRoutes.mjs";
+import Utility from "../utilities.mjs";
+import express from "express";
+import {describe, expect, test, jest} from "@jest/globals";
+
+jest.mock("../models/queue.mjs")
+jest.mock("../dao/serviceDAO.mjs")
 
 const baseURL = "/api/service";
 
-test ('POST /ticket', async () => {
-    const response = await new request(app)
-        .post(baseURL + '/ticket')
-        .send({
-        service: 'Money Transfer'
-        });
+describe("POST /ticket" , () => {
+    let app;
+    let mockServiceDAO;
+
+    beforeAll(() => {
+        app = express();
+        app.use(express.json());
+        app.use(Utility.errorHandler);
+        const serviceRoutes = new ServiceRoutes();
+        serviceRoutes.initRoutes();
+        app.use(baseURL, serviceRoutes.getRouter());
+
+
+    });
+
+    beforeEach(() => {
+        // Mock the serviceDAO methods directly
+        mockServiceDAO = new ServiceDAO();
+
+        jest.spyOn(mockServiceDAO, 'getServiceDetails');
+        jest.spyOn(mockServiceDAO, 'getServicesForAllCounters');
         
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toMatchObject({
-        ticket: "MT1",
-        estimatedWaitTime: 7.5,
-      });
-    }
-);
+        // Replace the real serviceDAO with the mock
+        ServiceRoutes.prototype.serviceDAO = mockServiceDAO;
+    });
+    
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    
+    test ('POST /ticket', async () => {
+
+        jest.spyOn(mockServiceDAO, 'getServiceDetails').mockResolvedValue({
+            id: 1,
+            name: 'Money Transfer',
+            code: 'MT',
+            averageTime: 15
+        });
+
+        const response = await request(app)
+            .post(baseURL + '/ticket')
+            .send({
+            service: 'Money Transfer'
+            });
+    
+        expect(mockServiceDAO.getServiceDetails).toHaveBeenCalledWith('money transfer')
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toMatchObject({
+            ticket: "MT1",
+            estimatedWaitTime: expect.any(Number),
+          });
+        });
+    });
+
+
+
+        
+
+
